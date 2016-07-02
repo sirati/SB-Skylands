0cbed99d92b763d043929f27ff51707c8c0a9820
sirati97 <TheSirati97@gmail.com>
2016-07-02 16:54:43 +0200

    improved speed

--- src/main/java/uk/co/jacekk/bukkit/skylandsplus/generation/LakePopulator.java
+++ src/main/java/uk/co/jacekk/bukkit/skylandsplus/generation/LakePopulator.java
@@ -1,54 +1,45 @@
 package uk.co.jacekk.bukkit.skylandsplus.generation;
 
+import de.sirati97.sb.skylands.BiomesUtil;
 import net.minecraft.server.v1_10_R1.BlockPosition;
 import net.minecraft.server.v1_10_R1.Blocks;
 import net.minecraft.server.v1_10_R1.WorldGenLakes;
 import net.minecraft.server.v1_10_R1.WorldGenReed;
 import org.bukkit.Chunk;
-import org.bukkit.block.Biome;
 import org.bukkit.craftbukkit.v1_10_R1.CraftWorld;
 import org.bukkit.generator.BlockPopulator;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class LakePopulator
-  extends BlockPopulator
-{
-  private Random random;
-  
-  public LakePopulator(org.bukkit.World world)
-  {
-    this.random = new Random(world.getSeed());
-  }
-  
-  public void populate(org.bukkit.World world, Random random, Chunk chunk)
-  {
-    net.minecraft.server.v1_10_R1.World mcWorld = ((CraftWorld)world).getHandle();
-    
-    int worldChunkX = chunk.getX() * 16;
-    int worldChunkZ = chunk.getZ() * 16;
-    if (this.random.nextInt(4) == 0)
-    {
-      int x = worldChunkX + this.random.nextInt(16) + 8;
-      int z = worldChunkZ + this.random.nextInt(16) + 8;
-      if (!Arrays.asList(new Biome[] { Biome.JUNGLE, Biome.JUNGLE_HILLS }).contains(world.getBiome(x, z)))
-      {
-        int y = world.getHighestBlockYAt(x, z) + 2;
-        BlockPosition pos = new BlockPosition(x,y,z);
-        if (this.random.nextInt(100) < 85)
-        {
-        	WorldGenLakes wgl = new WorldGenLakes(Blocks.WATER);
-        	wgl.generate(mcWorld, this.random, pos);
-        	WorldGenReed wgr = new WorldGenReed();
-        	wgr.generate(mcWorld, this.random, pos);
-        }
-        else
-        {
-        	WorldGenLakes wgl = new WorldGenLakes(Blocks.LAVA);
-        	wgl.generate(mcWorld, this.random, pos);
-        }
-      }
+        extends BlockPopulator {
+    private Random random;
+
+    public LakePopulator(org.bukkit.World world) {
+        this.random = new Random(world.getSeed());
     }
-  }
+
+    public void populate(org.bukkit.World world, Random random, Chunk chunk) {
+        net.minecraft.server.v1_10_R1.World mcWorld = ((CraftWorld) world).getHandle();
+
+        int worldChunkX = chunk.getX() * 16;
+        int worldChunkZ = chunk.getZ() * 16;
+        if (this.random.nextInt(4) == 0) {
+            int x = worldChunkX + this.random.nextInt(16) + 8;
+            int z = worldChunkZ + this.random.nextInt(16) + 8;
+            if (BiomesUtil.isJungle(world.getBiome(x, z))) {
+                int y = world.getHighestBlockYAt(x, z) + 2;
+                BlockPosition pos = new BlockPosition(x, y, z);
+                if (this.random.nextInt(100) < 85) {
+                    WorldGenLakes wgl = new WorldGenLakes(Blocks.WATER);
+                    wgl.generate(mcWorld, this.random, pos);
+                    WorldGenReed wgr = new WorldGenReed();
+                    wgr.generate(mcWorld, this.random, pos);
+                } else {
+                    WorldGenLakes wgl = new WorldGenLakes(Blocks.LAVA);
+                    wgl.generate(mcWorld, this.random, pos);
+                }
+            }
+        }
+    }
 }
--- src/main/java/uk/co/jacekk/bukkit/skylandsplus/generation/RedSandPopulator.java
+++ src/main/java/uk/co/jacekk/bukkit/skylandsplus/generation/RedSandPopulator.java
@@ -10,36 +10,29 @@ import org.bukkit.generator.BlockPopulator;
 
 import java.util.Random;
 
-public class RedSandPopulator extends BlockPopulator
-{
-	  public RedSandPopulator(){}
-	  
-	  @SuppressWarnings("deprecation")
-	  public void populate(World world, Random random, Chunk chunk)
-	  {
-		  for (int x = 0; x < 16; x++)
-		  {
-			  for (int z = 0; z < 16; z++)
-			  {
-				  for (int y = 0; y < 128; y++)
-				  {
-					  if (y > 4)
-					  {
-						  Block block = chunk.getBlock(x, y, z);
-						  Biome biome = block.getBiome();
-						  
-						  if (BiomesUtil.isMesa(biome) && block.getType().equals(Material.SAND))
-						  {
-							  chunk.getBlock(x, y, z).setData((byte) 1);
-						  }
-						  
-						  if (BiomesUtil.isMesa(biome) && (block.getType().equals(Material.DEAD_BUSH) || block.getType().equals(Material.LONG_GRASS)) || block.getType().equals(Material.CACTUS))
-						  {
-							  chunk.getBlock(x, y, z).setType(Material.AIR);
-						  }
-					  }
-				  }
-			  }
-		  }
-	  }
+public class RedSandPopulator extends BlockPopulator {
+    public RedSandPopulator() {
+    }
+
+    @SuppressWarnings("deprecation")
+    public void populate(World world, Random random, Chunk chunk) {
+        for (int x = 0; x < 16; x++) {
+            for (int z = 0; z < 16; z++) {
+                Biome biome = chunk.getBlock(x, 64, z).getBiome();
+                if (BiomesUtil.isMesa(biome)) {
+                    for (int y = 4; y < 128; y++) {
+                        Block block = chunk.getBlock(x, y, z);
+
+                        if (block.getType().equals(Material.SAND)) {
+                            block.setData((byte) 1);
+                        }
+
+                        if (block.getType().equals(Material.DEAD_BUSH) || block.getType().equals(Material.LONG_GRASS) || block.getType().equals(Material.CACTUS)) {
+                            block.setType(Material.AIR);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
